3829b4164154361adfad4ed65a5302d035a9a035
jacobhumston-school <jhumston24@sciototech.org>
2022-09-27 10:42:58 -0400

    Added icon.

--- projects/index.php
+++ projects/index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF8">
     <title>Project Files</title>
+    <link rel="icon" href="./Assets/folder_black_24dp.svg">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
