dd178e6dc93d721122de832d52107d7536b053fe
Jacob Humston <humstonjacob@gmail.com>
2022-10-10 23:00:42 -0400

    finnally got websocket going

--- projects/ChatApp/index.js
+++ projects/ChatApp/index.js
@@ -1,16 +1,21 @@
-const express = require("express")
-const cookieParser = require("cookie-parser")
-const app = express()
-const port = 4000
+// source code written by Jacob Humston, use for whatever u want!!!!
 
-app.use(cookieParser())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const app = express();
+const port = 4000;
+const webSocket = require("ws");
 
-const fs = require("fs")
-const { v4: uuidv4 } = require('uuid')
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const adminData = require("./adminpass.json")
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+// data is kept here in a very nice data object!!
+
+const adminData = require("./adminpass.json");
 
 const data = {
     accounts: [],
@@ -18,7 +23,7 @@ const data = {
     ipBlacklist: [],
     apiCooldowns: [],
     sessions: [],
-}
+};
 
 adminData.data.forEach(function (value) {
     data.accounts.push({
@@ -26,45 +31,47 @@ adminData.data.forEach(function (value) {
         password: value.pass,
         userId: uuidv4(),
         admin: true,
-        ipWhitelist: value.ip
-    })
-})
+        ipWhitelist: value.ip,
+    });
+});
 
-const blacklistedStrings = [" ", "<", ">", "/", "\\", "'", '"', "`"]
+// these are util functions
+
+const blacklistedStrings = [" ", "<", ">", "/", "\\", "'", '"', "`"];
 
 function hasBlacklistedString(string) {
-    let hasString = false
+    let hasString = false;
     blacklistedStrings.forEach(function (value) {
         if (string.includes(value) === true) {
-            hasString = true
+            hasString = true;
         }
-    })
-    return hasString
+    });
+    return hasString;
 }
 
 function isLoginValid(username, password, ip) {
-    if (typeof (username) === "string" && typeof (password) === "string") {
-        const responseObject = { exists: false, validUsername: true, validPassword: true, validLogin: false, ipPassed: true }
-        const foundIndex = data.accounts.findIndex(value => value.username === username)
+    if (typeof username === "string" && typeof password === "string") {
+        const responseObject = { exists: false, validUsername: true, validPassword: true, validLogin: false, ipPassed: true };
+        const foundIndex = data.accounts.findIndex((value) => value.username === username);
         if (foundIndex !== -1) {
-            responseObject.exists = true
+            responseObject.exists = true;
             if (data.accounts[foundIndex].password === password) {
-                responseObject.validLogin = true
+                responseObject.validLogin = true;
             }
             if (data.accounts[foundIndex].ipWhitelist !== null) {
                 if (data.accounts[foundIndex].ipWhitelist !== ip) {
-                    responseObject.ipPassed = false
+                    responseObject.ipPassed = false;
                 }
             }
-            responseObject.account = data.accounts[foundIndex]
+            responseObject.account = data.accounts[foundIndex];
         }
         if (username.length > 15 || username.length < 3 || hasBlacklistedString(username)) {
-            responseObject.validUsername = false
+            responseObject.validUsername = false;
         }
         if (password.length > 25 || password.length < 8 || hasBlacklistedString(password)) {
-            responseObject.validPassword = false
+            responseObject.validPassword = false;
         }
-        return responseObject
+        return responseObject;
     } else {
         return {
             exists: false,
@@ -72,116 +79,132 @@ function isLoginValid(username, password, ip) {
             validPassword: false,
             validLogin: false,
             ipPassed: false,
-        }
+        };
     }
 }
 
 function isIpBanned(ip) {
-    return data.ipBlacklist.includes(ip)
+    return data.ipBlacklist.includes(ip);
 }
 
 function isValidSession(sessionId) {
     if (sessionId !== null && sessionId !== undefined) {
-        if (data.sessions.findIndex(value => value.sessionId === sessionId) !== -1) {
-            return true
+        if (data.sessions.findIndex((value) => value.sessionId === sessionId) !== -1) {
+            return true;
         } else {
-            return false
+            return false;
         }
     } else {
-        return false
+        return false;
     }
 }
 
+// websocket for all the chat messenging needs
+
+const wsServer = new webSocket.Server({ port: 4001 });
+
+wsServer.on("listening", function () {
+    console.log("WebSocket is listening...");
+});
+
+wsServer.on("error", function (error) {
+    console.log("WebSocket Error: " + error.message);
+});
+
+wsServer.on("connection", function (webSocket, request) {});
+
+// this is the main api, used for login and such
+
 app.get("/", function (request, response) {
-    response.redirect("/chat")
-})
+    response.redirect("/chat");
+});
 
 app.get("/chat", function (request, response) {
     if (isIpBanned(request.ip) === true) {
-        response.send("You're IP is blacklisted, lol.")
+        response.send("You're IP is blacklisted, lol.");
     } else {
-        const sessionId = request.cookies.sessionId
+        const sessionId = request.cookies.sessionId;
         if (isValidSession(sessionId)) {
-            response.sendFile("./chatapp.html", { root: "./" })
+            response.sendFile("./chatapp.html", { root: "./" });
         } else {
-            response.redirect("/chat/login")
+            response.redirect("/chat/login");
         }
     }
-})
+});
 
 app.get("/chat/login", function (request, response) {
     if (isIpBanned(request.ip) === true) {
-        response.send("You're IP is blacklisted, lol.")
+        response.send("You're IP is blacklisted, lol.");
     } else {
-        const sessionId = request.cookies.sessionId
+        const sessionId = request.cookies.sessionId;
         if (isValidSession(sessionId)) {
-            response.redirect("/chat")
+            response.redirect("/chat");
         } else {
-            response.sendFile("./login.html", { root: "./" })
+            response.sendFile("./login.html", { root: "./" });
         }
     }
-})
+});
 
 app.post("/chat/login/api/signin", function (request, response) {
-    const body = request.body
+    const body = request.body;
     if (body === undefined || body === null) {
         response.send({
             success: false,
             errorType: "Invalid Login",
-            errorMessage: "Please provide a username and password."
-        })
+            errorMessage: "Please provide a username and password.",
+        });
     } else {
-        const loginValidation = isLoginValid(body.username, body.password, request.ip)
+        const loginValidation = isLoginValid(body.username, body.password, request.ip);
         if (loginValidation.exists) {
             if (loginValidation.validLogin) {
                 if (loginValidation.ipPassed) {
-                    const session = { sessionId: `${uuidv4()}-${uuidv4()}-${uuidv4()}`, accountId: loginValidation.account.userId }
-                    data.sessions.push(session)
-                    response.cookie("sessionId", session.sessionId)
+                    const session = { sessionId: `${uuidv4()}-${uuidv4()}-${uuidv4()}`, accountId: loginValidation.account.userId };
+                    data.sessions.push(session);
+                    response.cookie("sessionId", session.sessionId);
                     response.send({
                         success: true,
                         session: session,
-                    })
+                    });
                 } else {
                     response.send({
                         success: false,
                         errorType: "IP Whitelist",
-                        errorMessage: "This account can only be used by a specific IP."
-                    })
+                        errorMessage: "This account can only be used by a specific IP.<br>Debug: Your IP is " + request.ip,
+                    });
                 }
             } else {
                 response.send({
                     success: false,
                     errorType: "Invalid Login",
-                    errorMessage: "The username or password is incorrect."
-                })
+                    errorMessage: "The username or password is incorrect.",
+                });
             }
         } else {
             response.send({
                 success: false,
                 errorType: "Invalid Login",
-                errorMessage: "The username or password is incorrect."
-            })
+                errorMessage: "The username or password is incorrect.",
+            });
         }
     }
-})
+});
 
 app.post("/chat/login/api/signup", function (request, response) {
-    const body = request.body
+    const body = request.body;
     if (body === undefined || body === null) {
         response.send({
             success: false,
             errorType: "Invalid Login",
-            errorMessage: "Please provide a username and password."
-        })
+            errorMessage: "Please provide a username and password.",
+        });
     } else {
-        const loginValidation = isLoginValid(body.username, body.password, request.ip)
+        const loginValidation = isLoginValid(body.username, body.password, request.ip);
         if (loginValidation.exists) {
             response.send({
                 success: false,
                 errorType: "Account Already Exists",
-                errorMessage: "The username you picked is already being used, try logging in."
-            })
+                errorMessage: "The username you picked is already being used, try logging in.",
+            });
         } else {
             if (loginValidation.validPassword && loginValidation.validUsername) {
                 const newAccount = {
@@ -189,44 +212,49 @@ app.post("/chat/login/api/signup", function (request, response) {
                     password: body.password,
                     userId: uuidv4(),
                     admin: false,
-                    ipWhitelist: null
-                }
-                data.accounts.push(newAccount)
-                const session = { sessionId: `${uuidv4()}-${uuidv4()}-${uuidv4()}`, accountId: newAccount.userId }
-                data.sessions.push(session)
-                response.cookie("sessionId", session.sessionId)
+                    ipWhitelist: null,
+                };
+                data.accounts.push(newAccount);
+                const session = { sessionId: `${uuidv4()}-${uuidv4()}-${uuidv4()}`, accountId: newAccount.userId };
+                data.sessions.push(session);
+                response.cookie("sessionId", session.sessionId);
                 response.send({
                     success: true,
                     session: session,
-                })
+                });
             } else {
                 response.send({
                     success: false,
                     errorType: "Invalid Sign up",
                     errorMessage: `The username must be between 3-15 characters & the password must be between 8-25 characters.<br>Username and password cannot contain the following characters:&nbsp;<code>${blacklistedStrings.join("</code>, <code>")}</code>`,
-                    returned: loginValidation
-                })
+                    returned: loginValidation,
+                });
             }
         }
     }
-})
+});
 
+// add all asset urls
 for (const fileName of fs.readdirSync("./assets")) {
     app.get(`/chat/assets/${fileName}`, function (request, response) {
         response.sendFile(`./${fileName}`, {
             root: "./assets/",
-        })
-    })
+        });
+    });
 }
 
+// this is a dummy function for viewing data
+// it will get commented out when testing is done
 app.get("/data/get/lol", function (request, response) {
-    response.send(data)
-})
+    response.send(data);
+});
 
+// redirct to main page incase of 404
 app.use(function (request, response) {
-    response.redirect("/")
-})
+    response.redirect("/");
+});
 
+// we gotta listen if we wanna know whats going on
 app.listen(port, function () {
-    console.log(`Listening to port ${port}!`)
-})
+    console.log(`Listening to port ${port}!`);
+});
