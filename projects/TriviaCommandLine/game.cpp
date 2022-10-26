e3c70de8ba99654a1aa2268cb28837d98dca349d
jacobhumston-school <jhumston24@sciototech.org>
2022-10-26 10:55:01 -0400

    dfsfdsfsd

--- projects/TriviaCommandLine/game.cpp
+++ projects/TriviaCommandLine/game.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
-#include //...
+#include <stdio.h>
+#include <stdlib.h>
+#include <json.hpp>
+#include <fstream>
 
-//.. ima do this at home lol
+using namespace std;
+
+int main() {
+    system("clear");
+    char Data = system("curl https://opentdb.com/api.php?amount=1&type=multiple");
+    
+};
