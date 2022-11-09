a5de9e49804b2460ceb6f9a0f7b7e1b0ca11b51b
jacobhumston-school <jhumston24@sciototech.org>
2022-11-09 10:00:28 -0500

    eeheee

--- projects/Tutorials/HTMLButton/main.js
+++ projects/Tutorials/HTMLButton/main.js
@@ -1,7 +1,19 @@
-var Black = "rgb(0, 0, 0);";
-var White = "rgb(255, 255, 255);";
+var Black = "black"
+var White = "white"
 //  ^ Name  ^ Value
 
 function Loaded() {
+    var Button = document.getElementById("Button")
+    var Body = document.body
+    var LightTheme = true
 
+    Button.onclick = function() {
+        if (LightTheme == true) {
+            Body.style.backgroundColor = Black
+            LightTheme = !LightTheme // OR: LightTheme = false
+        } else {
+            Body.style.backgroundColor = White
+            LightTheme = !LightTheme // OR: LightTheme = true
+        }
+    }
 }
