96c343307e72edd37f3eb93d2593e5b72c5b66f7
jacobhumston-school <jhumston24@sciototech.org>
2022-12-07 09:48:32 -0500

    .

---
+++ projects/test.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!-- Authored by Jacob Humston, free for public use. -->
+
+<html>
+
+<head>
+    <title>Project Files - Jacob Humston</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Round" rel="stylesheet">
+    <style>
+    body,
+    html {
+        padding: 0px;
+        margin: 0px;
+        background-color: #2b2b2b;
+        color: #c7c7c7;
+    }
+
+    #Wrapper {
+        padding: 10px;
+    }
+    </style>
+</head>
+
+<body>
+    <div id="Wrapper">
+        <h1>Project Files</h1>
+        <p>By Jacob Humston</p>
+        <p>
+            <span class="material-icons-round">folder</span>Folder
+        </p>
+    </div>
+</body>
+
+</html>
