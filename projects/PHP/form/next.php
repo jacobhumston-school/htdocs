140921a5855022267238d67c0fe80c0a7f65b65c
jacobhumston-school <jhumston24@sciototech.org>
2023-03-20 10:52:28 -0400

    .

--- projects/PHP/form/next.php
+++ projects/PHP/form/next.php
@@ -1,15 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+    	<meta charset="UTF-8" />
+    	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    	<title>Forum Result</title>
+  	</head>
+  	<body>
+      <?php
 
-    $fName = $_POST['fname'];
-    echo $fName . "<br>";
+            $fName = $_POST['fname'];
+            echo $fName . "<br>";
 
-    $lName = $_POST['lname'];
-    echo $lName . "<br>";
+            $lName = $_POST['lname'];
+            echo $lName . "<br>";
 
-    $rName = $_POST['fav_race'];
-    echo $rName . "<br>";
+            $rName = $_POST['fav_race'];
+            echo $rName . "<br>";
 
-    $cName = $_POST['fav_class'];
-    echo $cName . "<br>";
+            $cName = $_POST['fav_class'];
+            echo $cName . "<br>";
 
-?>
+        ?>
+	</body>
+</html>
